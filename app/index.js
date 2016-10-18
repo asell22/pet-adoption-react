04aacc357f5f83969a714dfba80bc0108343b72c
asell22 <alexanderjsell@gmail.com>
2016-10-18 16:05:39 -0700

    Render App component

---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = function() {
+  return <div>Petfinder</div>
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
