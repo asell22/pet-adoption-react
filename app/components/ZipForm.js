817cffd4189c98edc6d861789125459ea0e04574
asell22 <alexanderjsell@gmail.com>
2016-10-24 14:31:48 -0700

    Add methods to ZipForm Component

--- app/components/ZipForm.js
+++ app/components/ZipForm.js
@@ -3,17 +3,32 @@ import { zipForm, button } from '../styles/zipForm';
 import Header from './Header';
 
 class ZipForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      zip: 'blah'
+    }
+  }
+  handleInputChange(evt) {
+    this.setState({
+      zip: evt.target.value
+    })
+  }
+  handleSubmit(evt) {
+    console.log('Zip:', this.state.zip)
+    evt.preventDefault();
+  }
   render() {
     return (
       <div>
         <Header text="Let Us Help You Find A Pet"/>
-        <form className="form-inline" style={zipForm}>
+        <form className="form-inline" style={zipForm} onSubmit={(evt) =>this.handleSubmit(evt)}>
           <h2 style={{fontFamily: 'sans-serif', fontWeight: '100'}}>Enter Your Zip Code</h2>
           <div className="form-group">
             <label htmlFor="zipCodeInput" className="sr-only">Enter Zip Code</label>
-            <input type="text" className="form-control" placeholder="Zip Code" required/>
+            <input type="text" className="form-control" placeholder="Zip Code" onChange={(evt) => this.handleInputChange(evt)} required/>
           </div>
-          <button className="btn btn-primary" type="submit" style={button}>Find Pets</button>
+          <button className="btn btn-primary" type="submit" style={button}  >Find Pets</button>
         </form>
       </div>
     )
