2c62188b0a02a94e55e9a13c88cd7f13935ccb7e
asell22 <alexanderjsell@gmail.com>
2016-10-24 15:39:08 -0700

    Add Form Component

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Main from '../components/Main';
 import ZipForm from '../components/ZipForm';
+import Form from '../components/Form';
 
 const routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={ZipForm}/>
+      <Route path='form' component={Form}/>
     </Route>
   </Router>
 )
