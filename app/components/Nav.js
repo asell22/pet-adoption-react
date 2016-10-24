ec0c81d79ab05ca311f35112150245f3b9d9949d
asell22 <alexanderjsell@gmail.com>
2016-10-24 11:34:37 -0700

    Nav component renders bootstrap default navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Nav = () => {
-  return <div>NAVIGATION BAR</div>
+  return (
+    <nav className="navbar navbar-default" style={{marginBottom: '10px'}}>
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="/">Petfinder</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav;
