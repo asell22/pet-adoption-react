6c48ac1dd5d6f3575a148dfdc57088a02af70dfb
asell22 <alexanderjsell@gmail.com>
2016-10-26 10:06:27 -0700

    Change back to select box add button click handler

--- app/components/Animal.js
+++ app/components/Animal.js
@@ -14,11 +14,14 @@ class AnimalContainer extends Component {
       animal: evt.target.value
     });
   }
+  handleButtonClick() {
+    console.log(this.state);
+  }
   render() {
     return (
       <div>
         <Header text="What kind of pet did you have in mind?"/>
-        {/* <div className="center" style={zipForm}>
+        <div className="center" style={zipForm}>
           <select className="form-control" onChange={(evt) => this.handleAnimalChange(evt)} >
             <option value="" style={{display:'none'}}>Select An Animal Type </option>
             <option value="barnyard">Barnyard</option>
@@ -30,21 +33,8 @@ class AnimalContainer extends Component {
             <option value="reptile">Reptile</option>
             <option value="smallfurry">Small And Furry</option>
           </select>
-        </div> */}
-        <div className="dropdown" style={{textAlign: 'center'}}>
-          <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style={{margin:'10px auto', padding: '10px 12px 10px 15px'}}>
-            Select An Animal Type
-            <span className="caret" style={{marginLeft: '20px'}} ></span>
-          </button>
-          <ul className="dropdown-menu" style={{margin:'10px auto', width: '200px', left: '50%', marginLeft:'-100px'}} aria-labelledby="dropdownMenu1">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" className="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
         </div>
-        <button className="btn btn-default center" style={btn}>Select</button>
+        <button className="btn btn-default center" style={btn} onClick={(evt) => this.handleButtonClick(evt)}>Select</button>
       </div>
 
     )
