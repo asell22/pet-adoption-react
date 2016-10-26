91b3b9a74046976f2c98fc3149ffecdd23d2d7ab
asell22 <alexanderjsell@gmail.com>
2016-10-26 11:14:06 -0700

    Add PropTypes to Animal Component

--- app/components/Animal.js
+++ app/components/Animal.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Header from './Header';
 import { zipForm, btn } from '../styles/zipForm';
 
-const Animal = (props) => {
+const Animal = ({ onButtonClick, onAnimalChange }) => {
   return (
     <div>
       <Header text="What kind of pet did you have in mind?"/>
       <div className="center" style={zipForm}>
-        <select className="form-control" onChange={props.onAnimalChange}>
+        <select className="form-control" onChange={onAnimalChange}>
           <option value="" style={{display:'none'}}>Select An Animal Type </option>
           <option value="barnyard">Barnyard</option>
           <option value="bird">Bird</option>
@@ -19,9 +19,14 @@ const Animal = (props) => {
           <option value="smallfurry">Small And Furry</option>
         </select>
       </div>
-      <button className="btn btn-default center" style={btn} onClick={props.onButtonClick}>Select</button>
+      <button className="btn btn-default center" style={btn} onClick={onButtonClick}>Select</button>
     </div>
   )
+}
+
+Animal.propTypes = {
+  onAnimalChange: PropTypes.func.isRequired,
+  onButtonClick: PropTypes.func.isRequired
 }
 
 export default Animal;
