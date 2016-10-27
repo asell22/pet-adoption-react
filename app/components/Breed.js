f8084d61bd961a85f2b9793fbb1f19b5e638ac53
asell22 <alexanderjsell@gmail.com>
2016-10-27 10:07:31 -0700

    API request to get breed types

--- app/components/Breed.js
+++ app/components/Breed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_KEY } from '../../keys';
 
 class Breed extends Component {
   constructor(props) {
@@ -13,12 +14,29 @@ class Breed extends Component {
   }
   componentDidMount() {
     console.log('STATE:', this.state)
+    const url = `http://api.petfinder.com/breed.list?format=json&key=${API_KEY}&animal=${this.state.animal}`;
+    $.ajax({
+      type : 'GET',
+      data : {},
+      url : url+'&callback=?' ,
+      dataType: 'json'
+    }).then((data) => {
+      console.log(data);
+      this.setState({
+        isLoading: false,
+        breeds: data.petfinder.breeds.breed
+      })
+    }).then(() => {
+      console.log(this.state);
+    })
+
+
     // Fetch breeds from PETFINDER API, then update state
   }
   render() {
     return this.state.isLoading === true
       ? <p>LOADING</p>
-      : <p>BREED COMPONENT</p> 
+      : <p>BREED COMPONENT</p>
   }
 }
 
