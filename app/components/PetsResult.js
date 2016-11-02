e87da9c0ec17dc226836342b4e32e6b0ee12f305
asell22 <alexanderjsell@gmail.com>
2016-11-02 14:58:56 -0700

    Add PetsResult Component

---
+++ app/components/PetsResult.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PetsResult = ({pets}) => {
+  return (
+    <div>
+      {pets.map((pet, indx) =>
+      <div className="col-sm-3" style={{textAlign: 'center', margin: '40px 0px'}} key={indx.toString()}>
+        <div style={{ cursor: 'pointer'}}>
+          {Object.keys(pet.media).length !== 0 ? <img src={pet.media.photos.photo[2].$t} width="150" height="150" alt="" style={{borderRadius: '5%', objectFit: 'cover'}}/> : <div ><span style={{border: '1px solid #000', padding: '50px 0', display: 'block', margin: '20px', background: '#ccc', borderRadius: '2%',  cursor: 'pointer'}}>NO PHOTO</span></div>}
+          <h5>Meet {pet.name.$t}</h5>
+        </div>
+      </div>
+      )}
+    </div>
+
+  )
+}
+
+export default PetsResult;
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { API_KEY } from '../../keys';
+import PetsResult from './PetsResult';
 
 class Results extends Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class Results extends Component {
 
   componentDidMount() {
     http://api.petfinder.com/pet.find?key=92c1ab693c2e94984374d634551c27ab&animal=dog&breed=Boxer&size=S&age=Young&sex=F&location=94952&description=full&format=json
-    const url = `http://api.petfinder.com/pet.find?key=${API_KEY}&animal=${this.state.animal}&breed=${this.state.breed}&size=${this.state.size}&age=${this.state.age}&sex=${this.state.gender}&location=${this.state.zip}&description=full&format=json`;
+    const url = `http://api.petfinder.com/pet.find?key=${API_KEY}&animal=${this.state.animal}&breed=${this.state.breed}&size=${this.state.size}&age=${this.state.age}&sex=${this.state.gender}&location=${this.state.zip}&description=full&format=json&count=300`;
     console.log('URL', url);
 
     $.ajax({
@@ -18,23 +19,26 @@ class Results extends Component {
       url : url+'&callback=?',
       dataType: 'json'
     }).then((data) => {
-      // console.log('RESULTS:', data.petfinder.pets.pet)
-      console.log('DATA:', data);
-      this.setState({
-        pets: [].concat(data.petfinder.pets.pet)
-      });
-      console.log('PETS:', this.state.pets)
+      console.log('DATA:', data.petfinder.pets);
+      if (Object.keys(data.petfinder.pets).length === 0) {
+        this.setState({
+          pets: undefined
+        })
+      } else {
+        this.setState({
+          pets: [].concat(data.petfinder.pets.pet)
+        });
+        console.log('PETS:', this.state.pets)
+      }
+
     })
   }
 
   render() {
     return (
       <div className="row">
-        {this.state.pets !== undefined ? this.state.pets.map((pet, indx) =>
-          <div className="col-sm-4" style={{textAlign: 'center', margin: '40px 0px'}} key={indx.toString()}>
-            {Object.keys(pet.media).length !== 0 ? <img src={pet.media.photos.photo[2].$t} width="150" height="150" alt="" style={{borderRadius: '5%', objectFit: 'cover'}}/> : <div ><span style={{border: '1px solid #000', padding: '50px 0', display: 'block', margin: '20px', background: '#ccc', borderRadius: '2%'}}>NO PHOTO</span></div>}
-            <h5>Meet {pet.name.$t}</h5>
-          </div>) :
+        {this.state.pets !== undefined ?
+          <PetsResult pets={this.state.pets}/> :
           <h2>Sorry no Matches</h2>
         }
       </div>
