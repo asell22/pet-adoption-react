db7809fe0a2a7f15e8a437bef809c2acc05dcae4
asell22 <alexanderjsell@gmail.com>
2016-11-03 13:49:28 -0700

    Conditional for Pets that don't have photos

--- app/components/Pet.js
+++ app/components/Pet.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Pet = (props) => {
   console.log(props.location.state.pet);
-  const { description } = props.location.state.pet
-  return <div>{description.$t}</div>
+  const { description, contact, name, media  } = props.location.state.pet
+  if (media.photos) {
+    return (
+      <div>
+        <img src={media.photos.photo[2].$t} alt="" />
+        <h3>This is {name.$t}</h3>
+        <p>{description.$t}</p>
+      </div>
+    )
+    } else {
+      return <div>No Photo</div>
+    }
 }
 
 export default Pet;
