1daf223a331f0b0bbbb12700b272982e5f9ef308
asell22 <alexanderjsell@gmail.com>
2016-11-04 11:29:38 -0700

    Render contact info in Pet Component

--- app/components/Pet.js
+++ app/components/Pet.js
@@ -5,10 +5,25 @@ const Pet = (props) => {
   const { description, contact, name, media  } = props.location.state.pet
   if (media.photos) {
     return (
-      <div>
-        <img src={media.photos.photo[2].$t} alt="" />
-        <h3>This is {name.$t}</h3>
-        <p>{description.$t}</p>
+      <div className="row">
+        <div className="col-sm-5">
+          <img style={{borderRadius: '5%'}}src={media.photos.photo[2].$t} alt="" />
+        </div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-5">
+          <h3>This is {name.$t}</h3>
+          <p>{description.$t}</p>
+          { contact.email.$st !== "" ?
+            <div>
+              <a href={`mailto:${contact.email.$t}`}>{contact.email.$t}</a>
+              <h5>{contact.phone.$t}</h5>
+            </div>
+          :
+            <h5>{contact.phone.$t}</h5>
+          }
+
+        </div>
+
       </div>
     )
     } else {
