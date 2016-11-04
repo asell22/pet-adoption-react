f0290eeebe9fa16c8733e1fb9274ac8fc59d0133
asell22 <alexanderjsell@gmail.com>
2016-11-04 14:32:07 -0700

    Store pet with localStorage API on button click

--- app/components/Pet.js
+++ app/components/Pet.js
@@ -10,7 +10,12 @@ class Pet extends Component {
   }
 
   handleButtonClick() {
-    console.log(this.state.pet)
+    const petStr = JSON.stringify(this.state.pet);
+    const id = this.state.pet.id.$t;
+    console.log('ID:', id);
+    console.log('petStr:', petStr);
+    localStorage.setItem(id, petStr);
+    console.log('item saved')
   }
 
   render() {
@@ -35,10 +40,7 @@ class Pet extends Component {
               </div>
             }
             <button style={{display: 'inline-block', margin: '0 0 40px 150px'}} className="btn btn-primary" onClick={() => this.handleButtonClick()}>Add To List</button>
-
-
           </div>
-
         </div>
       )
       } else {
@@ -46,39 +48,5 @@ class Pet extends Component {
       }
   }
 }
-// const Pet = (props) => {
-//   console.log(props.location.state.pet);
-//   const { description, contact, name, media, id } = props.location.state.pet
-//   if (media.photos) {
-//     return (
-//       <div className="row">
-//         <div className="col-sm-5">
-//           <img style={{borderRadius: '5%'}}src={media.photos.photo[2].$t} alt="" />
-//         </div>
-//         <div className="col-sm-1"></div>
-//         <div className="col-sm-5">
-//           <h3>This is {name.$t}</h3>
-//           <p>{description.$t}</p>
-//           { contact.email.$st !== ""   ?
-//             <div style={{display: 'inline-block'}}>
-//               <a href={`mailto:${contact.email.$t}`}>{contact.email.$t}</a>
-//               <h5>{contact.phone.$t}</h5>
-//             </div>
-//           :
-//             <div>
-//               <h5>{contact.phone.$t}</h5>
-//             </div>
-//           }
-//           <button style={{display: 'inline-block', margin: '0 0 40px 150px'}} className="btn btn-primary" onClick={() => buttonClick()}>Add To List</button>
-//
-//
-//         </div>
-//
-//       </div>
-//     )
-//     } else {
-//       return <div>No Photo</div>
-//     }
-// }
 
 export default Pet;
