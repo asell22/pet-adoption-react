c9e08fec4059b9f231da1960a4c001c3b73513bb
asell22 <alexanderjsell@gmail.com>
2016-11-05 14:42:58 -0700

    Render Pet Image in UserPets Component

--- app/components/UserPets.js
+++ app/components/UserPets.js
@@ -27,11 +27,23 @@ class UserPets extends Component {
       <div>
         <h4>List of Pets</h4>
         {this.state.savedPets.map((pet, indx, arr) => {
-          return <div key={arr[indx].id.$t}>{arr[indx].name.$t}</div>
+          return (
+            <div className="col-sm-3" key={arr[indx].id.$t} style={{textAlign: 'center', margin: '40px 0px'}}>
+              <div style={{cursor: 'pointer'}}>
+                {Object.keys(arr[indx]).length !== 0 ?
+                  <img src={arr[indx].media.photos.photo[2].$t} width="150" height="150" style={{borderRadius: '5%', objectFit: 'cover'}}/>
+                  :
+                  <div>
+                    <span style={{border: '1px solid #000', padding: '70px 0', display: 'block', background: '#ccc', borderRadius: '2%',  cursor: 'pointer'}}>NO PHOTO</span>
+                  </div>
+                }
+              </div>
+              {arr[indx].name.$t}
+            </div>
+          )
         })}
-      </div>
+        </div>
     )
-
   }
 }
 
